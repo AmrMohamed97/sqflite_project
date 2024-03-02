3bc15023987e2edc08ba29ef53b04a27753e0633
AmrMohamed97 <https://AmrMohamed97@github.com>
2024-03-02 13:21:49 +0200

    edit file

--- lib/core/utiles/functions/components.dart
+++ lib/core/utiles/functions/components.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-
+// Amr
  Widget  buildTaskItem(context , Map model) =>Dismissible(
     key: UniqueKey(),
     onDismissed: (direction){
